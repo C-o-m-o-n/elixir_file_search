c26d50e109d9960b5068f1044da5995f40cfbad1
c-o-m-o-n <comon928@gmail.com>
2024-06-30 22:07:44 +0300

    add loader

--- lib/file_search.ex
+++ lib/file_search.ex
@@ -3,21 +3,14 @@ defmodule FileSearch do
   Documentation for `FileSearch`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FileSearch.hello()
-      :world
-
-  """
   def main(args) do
     {opts, _, _} = OptionParser.parse(args, switches: [path: :string, query: :string])
     
     case opts do
       [path: path, query: query] ->
-      search_files(path, query)
+      {:ok, pid} = Loader.start_link([])
+        search_files(path, query)
+        GenServer.stop(pid)
       _-> IO.puts("Usage: file_search --path PATH --query QUERY")
       end
   end
@@ -28,7 +21,17 @@ defmodule FileSearch do
     |> Path.join("**/*")
     |> Path.wildcard()
     |> Enum.filter(&File.regular?/1)
-    |> Enum.filter(&String.contains?(&1, query))
-    |> Enum.each(&IO.puts/1)
+    # |> Enum.filter(&String.contains?(&1, query))
+    # |> Enum.each(&IO.puts/1)
+    |> Enum.with_index()
+    |> Enum.each(fn {file, index} ->
+      if rem(index, 100) == 0 do
+        IO.puts("Processed #{index} files...")
+      end
+
+      if String.contains?(file, query) do
+        IO.puts(file)
+      end
+    end)
   end
 end
---
+++ lib/loader.ex
@@ -0,0 +1,26 @@
+defmodule Loader do
+  use GenServer
+
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def init(:ok) do
+    schedule_work()
+    {:ok, :ok}
+  end
+
+  def handle_info(:work, state) do
+    print_spinner()
+    schedule_work()
+    {:noreply, state}
+  end
+
+  defp schedule_work do
+    Process.send_after(self(), :work, 100)
+  end
+
+  defp print_spinner do
+    IO.write(".")
+  end
+end
